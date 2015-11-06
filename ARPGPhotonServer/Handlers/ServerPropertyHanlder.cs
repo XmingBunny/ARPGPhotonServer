2cfe0a7aafd85d7e1b9648c0d0cddfedfc67320b
Siuming <2457944792@qq.com>
2015-11-06 17:15:19 +0800

    新增了服务器列表请求的处理Handler

---
+++ ARPGPhotonServer/Handlers/ServerPropertyHanlder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ARPGCommon;
+using ARPGPhotonServer.DB.Manager;
+using LitJson;
+using Photon.SocketServer;
+
+namespace ARPGPhotonServer.Handlers
+{
+    internal class ServerPropertyHanlder : HandlerBase
+    {
+        private ServerPropertyManager _manager;
+
+        public ServerPropertyHanlder()
+        {
+            this._manager = new ServerPropertyManager();
+        }
+
+        public override OperationResponse OnHandlerMeesage(OperationRequest request)
+        {
+            var list = _manager.GetserverpropertyList();
+            var json = JsonMapper.ToJson(list);
+            var parameters = new Dictionary<byte, object> {{(byte) ParameterCode.ServerList, json}};
+
+            var response = new OperationResponse(request.OperationCode, parameters)
+            {
+                ReturnCode = (short) ReturnCode.Success
+            };
+            return response;
+        }
+    }
+}

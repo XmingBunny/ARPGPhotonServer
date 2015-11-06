7da8068dd0913f007340f51cc6e4b151151b18cb
Siuming <2457944792@qq.com>
2015-11-06 17:10:25 +0800

    增加了一个Model,是和数据库中数据相对应的数据类

---
+++ ARPGCommon/Model/ServerProperty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ARPGCommon.Model
+{
+    public class ServerProperty
+    {
+        public virtual string Id { get; set; }
+        public virtual string Ip { get; set; }
+        public virtual string Name { get; set; }
+        public virtual int Count { get; set; }
+    }
+}

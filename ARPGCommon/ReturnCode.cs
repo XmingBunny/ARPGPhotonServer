3c84be352629646d5ac8a97eb0cb22b9481b98f2
Siuming <2457944792@qq.com>
2015-11-06 17:09:48 +0800

    第一次添加，表示请求的处理结果，有成功失败，错误和异常 错误和异常说明程序存在问题

---
+++ ARPGCommon/ReturnCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ARPGCommon
+{
+    public enum ReturnCode : byte
+    {
+        Success,
+        Fail,
+        Error,
+        Eception
+    }
+}
